acc07d366e66d8e2a7e0b454d8c2a7b53c2128a0
Arjun Poudel <arjunpoudel479@gmail.com>
2026-07-20 19:48:01 +0545

    ..

---
+++ src/app/api/whop-checkout/webhook-notifications/route.js
@@ -0,0 +1,32 @@
+import { waitUntil } from "@vercel/functions";
+import { Whop } from "@whop/sdk";
+
+export const whopsdk = new Whop({
+   	apiKey: process.env.WHOP_API_KEY,
+   	webhookKey: btoa(process.env.WHOP_WEBHOOK_SECRET || ""),
+});
+
+export async function POST(request) {
+
+  // Validate the webhook to ensure it's from Whop
+  const requestBodyText = await request.text();
+  const headers = Object.fromEntries(request.headers);
+  const webhookData = whopsdk.webhooks.unwrap(requestBodyText, { headers });
+
+  // Handle the webhook event
+  if (webhookData.type === "payment.succeeded") {
+    waitUntil(handlePaymentSucceeded(webhookData.data));
+  }
+  
+  // Make sure to return a 2xx status code quickly.
+  // Otherwise the webhook will be retried.
+  return new Response("OK", { status: 200 });
+
+}
+
+async function handlePaymentSucceeded(invoice) {
+  // This is a placeholder for a potentially long-running operation.
+  // In a real scenario, you might fetch user data, update a database, etc.
+  console.log("[PAYMENT SUCCEEDED]", invoice);
+
+}
